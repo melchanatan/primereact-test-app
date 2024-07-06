1e319e4b278a2bd0aa073b355537dbcf32bc5a88
melchanatan <chanatan@outlook.com>
2024-07-06 11:09:15 +0700

    feat: added LoadSkeleton on page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,11 @@ import { Button } from "primereact/button";
 import { Toast } from "primereact/toast";
 import ItemCard from "@/components/ItemCard";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { Skeleton } from "primereact/skeleton";
 
 export default function Home() {
   const toast = useRef(null);
-  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 
   const show = () => {
     toast.current.show({
@@ -17,20 +18,30 @@ export default function Home() {
     });
   };
 
-  const fetchMoreData = () => {
-    setItems([...items, ...Array.from({ length: 10 }, (_, i) => i + 1)]);
+  const fetchMoreData = async () => {
+    await new Promise((r) => setTimeout(r, 1500));
+
+    setItems([...items, ...Array.from({ length: 6 }, (_, i) => i + 1)]);
   };
+
+  const ItemLoadingSkeleton = (
+    <>
+      <div className="animate-pulse w-full max-w-[518px] h-[200px] bg-gray-300 rounded-lg"></div>
+      <div className="animate-pulse w-full max-w-[518px] h-[200px] bg-gray-300 rounded-lg"></div>
+      <div className="animate-pulse w-full max-w-[518px] h-[200px] bg-gray-300 rounded-lg"></div>
+    </>
+  );
 
   return (
     <>
       <Toast ref={toast} />
       <main>
         <InfiniteScroll
-          className="grid grid-cols-2 md:grid-cols-3 items-start justify-start px-5 gap-5"
+          className="grid grid-cols-2 md:grid-cols-3 items-start justify-center px-5 gap-5 [&>*]:justify-self-center"
           dataLength={items.length}
           next={fetchMoreData}
           hasMore={true}
-          loader={<h4>Loading...</h4>}
+          loader={ItemLoadingSkeleton}
         >
           {items.map((i, index) => (
             <ItemCard showToast={show} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,55 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { Menubar } from "primereact/menubar";
-
+import { Menu } from "primereact/menu";
+import { Button } from "primereact/button";
 import { Avatar } from "primereact/avatar";
 import "primeicons/primeicons.css";
 import Cart from "./Cart";
 
 const Navbar = () => {
   const start = <img src="/logo.png" alt="" className="w-7 aspect-square" />;
+  const menuRight = useRef(null);
+  const items = [
+    {
+      label: "Options",
+      items: [
+        {
+          label: "Refresh",
+          icon: "pi pi-refresh",
+        },
+        {
+          label: "Export",
+          icon: "pi pi-upload",
+        },
+      ],
+    },
+  ];
 
   const end = (
     <div className="flex items-center gap-4">
       <Cart />
-      <div class="flex flex-col items-end ml-[4vw]">
-        <p>melchs_</p>
-        <p className="text-sm text-gray-400">@chanatanmel</p>
-      </div>
-      <Avatar
-        label="V"
-        size="small"
-        style={{ backgroundColor: "#2196F3", color: "#ffffff" }}
+      <Button
+        text
+        onClick={(event) => menuRight.current.toggle(event)}
+        className="flex gap-3 px-2 py-1"
+      >
+        <div class="flex flex-col items-end">
+          <p>melchs_</p>
+          <p className="text-sm text-gray-400">@chanatanmel</p>
+        </div>
+        <Avatar
+          label="V"
+          size="small"
+          style={{ backgroundColor: "#2196F3", color: "#ffffff" }}
+        />
+      </Button>
+      <Menu
+        model={items}
+        popup
+        ref={menuRight}
+        id="popup_menu_right"
+        popupAlignment="right"
       />
     </div>
   );
